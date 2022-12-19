6735576a5cf95d2e5281e02c6d54af9953539837
Ashwin Soni <ashwinsoni1007@gmail.com>
2022-12-19 15:33:40 +0530

    commiting initial code

---
+++ build/precache-manifest.bce5711894424687c040e95759f7726e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e105d1f139da4127a8cf84a560fd6d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df1ff068500beaceb2b7",
+    "url": "/static/js/2.e7251730.chunk.js"
+  },
+  {
+    "revision": "12822a7f26afe9039854",
+    "url": "/static/js/main.a18b9cbf.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
